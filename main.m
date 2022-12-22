b1ecc6060b2da019d1eda3287b4dea9ea3818415
John Emile <john.emile16@gmail.com>
2022-12-22 23:24:13 +0200

    Requirement 3 done
    Added white additive guassian noise with variance 0.1 and plotted it with time.

--- main.m
+++ main.m
@@ -3,8 +3,8 @@ clear all;
 close all;
 
 %% Initialize variables
-x_t = []; 
-x_tg = []; %guard band added 
+x_tng = []; %no guard band 
+x_t = []; %guard band added 
 Fs = 8000; %Sampling frequency = 800Hz
 % T = 1/8000 sec/sample
 % 1/8000 ---> 1
@@ -14,28 +14,35 @@ Ng = 160; %Guard band of 20ms
 silence = zeros(1,Ng);
 phoneNum = '01284299877';
 for i=1:length(phoneNum)
-    x_t = [x_t Sym2TT(phoneNum(i))];
+    x_tng = [x_tng Sym2TT(phoneNum(i))];
 end
 
 %% Plotting signal amplitude with time
 %Range of time axis [0 to length(phone_num) with step length(x_t)]
-t = linspace(0,length(phoneNum),length(x_t));
+time = linspace(0,length(phoneNum),length(x_tng));
 figure(1)
-plot (t,x_t)
+plot (time,x_tng)
 title ('Input Signal')
 xlabel('Time(s)')
 ylabel('Amplitude')
 
 %% Adding guard band of 20ms and plot it with time
 for i=1:length(phoneNum)
-x_tg = [x_tg Sym2TT(phoneNum(i)) silence]; %create gap between digits
+x_t = [x_t Sym2TT(phoneNum(i)) silence]; %create gap between digits
 end
 figure(2)
 title ('Input Signal with guard band of 20ms')
 xlabel('Time(s)')
 ylabel('Amplitude')
-tg = linspace(0,length(phoneNum),length(x_tg));
-plot(tg,x_tg)
+t = linspace(0,length(phoneNum),length(x_t));
+plot(t,x_t)
 
-
-
+%% Making white additive guassian noise with variance = 0.1 and mean =0
+var = 1; %variance of additive white guassian noise
+noise = sqrt(var)*randn(size(t));
+y_t = x_t + noise;
+figure(3)
+title ('AGWN + Signal')
+xlabel('Time(s)')
+ylabel('Amplitude')
+plot(t,y_t)
