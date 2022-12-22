1f2817fcaf8d49c3e758c96cd427ee71a369fb2a
John Emile <john.emile16@gmail.com>
2022-12-22 22:37:20 +0200

    Update Trialanderror.m
    -Added some comments to specify how we get number of samples(N)
    -Specified the x axis (time) range.

--- Trialanderror.m
+++ Trialanderror.m
@@ -2,15 +2,21 @@ clc;
 clear all;
 close all;
 
-x_t = [];
-Fs = 8000;
-phone_num = '01284299877';
-for i=1:length(phone_num)
-    x_t = [x_t Sym2TT(phone_num(i))];
+x_t = []; 
+Fs = 8000; %Sampling frequency = 800Hz
+% T = 1/8000 sec/sample
+% 1/8000 ---> 1
+% 100ms  ---> N
+N = 800; 
+
+phoneNum = '01284299877';
+for i=1:length(phoneNum)
+    x_t = [x_t Sym2TT(phoneNum(i))];
 end
 
-t = linspace(0,0.12*length(phone_num),length(x_t));
+%Range of time axis [0 to length(phone_num) with step length(x_t)]
+t = linspace(0,length(phoneNum),length(x_t));
 plot (t,x_t)
 title ('input signal')
-xlabel('Time')
-ylabel('Amp')
+xlabel('Time(s)')
+ylabel('Amplitude')
